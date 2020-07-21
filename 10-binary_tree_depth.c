a8f23be177b18c95f69c535c86287fa9bfb2545a
David Orozco <dvdizcky@gmail.com>
2020-07-21 12:09:24 -0500

    10 added

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,12 +3,9 @@
 
 size_t depth(const binary_tree_t *tree, size_t depth_t)
 {
-	int l, r;
 	if (tree)
 	{
-		l = depth(tree->left, depth_t + 1);
-		r = depth(tree->right, depth_t + 1);
-		return l > r? l: r;
+		return depth(tree->parent, depth_t + 1);
 	}
 
 	return (depth_t);
@@ -20,6 +17,6 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (depth(tree, 0));
+	return (depth(tree, 0) - 1);
 }
 
